158c6e9275793fcbcad098807ba864f397c7bbec
Iman Mahmoudinasab <imanmahmoudinasab@gmail.com>
2024-09-14 15:38:45 -0700

    logo and header updated

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Container, Group, Burger } from "@mantine/core";
 import { Button } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
-import logo from "./logo.svg";
+import logo from "./logo.png";
 import classes from "./Header.module.css";
 
 const links = [
@@ -14,10 +14,12 @@ const links = [
 
 export function Header() {
   return (
-    <header className="p-4">
+    <header className="p-4 border-b border-gray-200">
       <Container size="xl" className={classes.inner}>
-        <div className="flex">
-          <img src={logo} className="h-9 w-9" alt="logo" />
+        <div className="flex items-center gap-1">
+          <div>
+            <img src={logo} className="h-7 w-7" alt="logo" />
+          </div>
           <h1 class="text-2xl font-bold">Tech Hive</h1>
         </div>
         <Button>Login</Button>
