106dc88f37ffe004be3b5b3d57fae8bc02da0fd5
Iman Mahmoudinasab <imanmahmoudinasab@gmail.com>
2024-09-14 19:41:03 -0700

    Change event names

--- src/Events/Event.jsx
+++ src/Events/Event.jsx
@@ -1,36 +1,57 @@
 import React from "react";
-import { Card, Text } from '@mantine/core';
+import { Card, Text } from "@mantine/core";
 import eventPicture from "./eventPicture.png";
 import { ActionIcon } from "@mantine/core";
 import { IconShare } from "@tabler/icons-react";
 
-export function Event({ name }) {
-    return (
-        <Card shadow="sm" padding="lg" radius="md" className="mb-4" withBorder>
-            <Card.Section>
-                <img
-                    src={eventPicture}
-                    height={160}
-                />
-            </Card.Section>
-            <div className="flex justify-between items-center mt-2">
-                <div className="flex flex-wrap gap-2">
-                    <Text size="xs" fw={500} className="bg-gray-500 text-white px-2 py-1 rounded">SEO</Text>
-                    <Text size="xs" fw={500} className="bg-gray-500 text-white px-2 py-1 rounded">Workshop</Text>
-                    <Text size="xs" fw={500} className="bg-gray-500 text-white px-2 py-1 rounded">Technical</Text>
-                </div>
-                <Text size="xs" fw={700} className="text-align-right">Free</Text>
-            </div>
-            <div>
-                <Text fw={700} mt="sm">TECHNICAL SEO WORKSHOP</Text>
-                <Text className="text-gray-500 text-[10px]">Description</Text>
-            </div>
-            <div className="flex justify-between items-center mt-2">
-                <Text size="xs" className="text-gray-500">Sep/14/2024 12:00 - 13:00</Text>
-                <ActionIcon variant="subtle" color="dark" aria-label="Share">
-                    <IconShare size={16} />
-                </ActionIcon>
-            </div>
-        </Card>
-    );
+export function Event({ event }) {
+  return (
+    <Card shadow="sm" padding="lg" radius="md" className="mb-4" withBorder>
+      <Card.Section>
+        <img src={eventPicture} height={160} />
+      </Card.Section>
+      <div className="flex justify-between items-center mt-2">
+        <div className="flex flex-wrap gap-2">
+          <Text
+            size="xs"
+            fw={500}
+            className="bg-gray-500 text-white px-2 py-1 rounded"
+          >
+            SEO
+          </Text>
+          <Text
+            size="xs"
+            fw={500}
+            className="bg-gray-500 text-white px-2 py-1 rounded"
+          >
+            Workshop
+          </Text>
+          <Text
+            size="xs"
+            fw={500}
+            className="bg-gray-500 text-white px-2 py-1 rounded"
+          >
+            Technical
+          </Text>
+        </div>
+        <Text size="xs" fw={700} className="text-align-right">
+          Free
+        </Text>
+      </div>
+      <div>
+        <Text fw={700} mt="sm">
+          {event.name}
+        </Text>
+        <Text className="text-gray-500 text-[10px]">Description</Text>
+      </div>
+      <div className="flex justify-between items-center mt-2">
+        <Text size="xs" className="text-gray-500">
+          Sep/14/2024 12:00 - 13:00
+        </Text>
+        <ActionIcon variant="subtle" color="dark" aria-label="Share">
+          <IconShare size={16} />
+        </ActionIcon>
+      </div>
+    </Card>
+  );
 }
--- src/Events/Events.jsx
+++ src/Events/Events.jsx
@@ -1,44 +1,48 @@
 import React from "react";
 import { Event } from "./Event";
-import { Button, ActionIcon } from '@mantine/core';
-import { IconTriangleInvertedFilled, IconFilter } from '@tabler/icons-react';
+import { Button, ActionIcon } from "@mantine/core";
+import { IconTriangleInvertedFilled, IconFilter } from "@tabler/icons-react";
 
 const events = [
-    {
-        id: 1,
-        name: "Vancouver Jobs",
-    },
-    {
-        id: 2,
-        name: "Vancouver Startups",
-    },
+  {
+    id: 1,
+    name: "BEGINNERS: TECHNICAL SEO WORKSHOP",
+  },
+  {
+    id: 2,
+    name: "INTERMEDIATE: TECHNICAL SEO WORKSHOP ",
+  },
+  {
+    id: 3,
+    name: "ADVANCED: TECHNICAL SEO WORKSHOP ",
+  },
 ];
 
 export function Events() {
-    return (
-        <div className="w-[400px] p-8">
-            <div className="flex justify-between mb-4">
-                <div className="flex gap-2 items-center">
-                    <h1 className="text-2xl font-bold">Events</h1>
-                    <IconTriangleInvertedFilled size={12} />
-                </div>
-                <div className="flex gap-2 items-center">
-                    <Button variant="light">POPULAR</Button>
-                    <ActionIcon
-                        variant="white"
-                        size="lg"
-                        color="dark"
-                        aria-label="add feeds"
-                    >
-                        <IconFilter />
-                    </ActionIcon>
-                </div>
-            </div>
-            <div>
-                {events.map((event) => {
-                    return <Event key={event.id} event={event} />;
-                })}
-            </div>
+  return (
+    <div className="w-[400px] p-8">
+      <div className="flex justify-between mb-4">
+        <div className="flex gap-2 items-center">
+          <h1 className="text-2xl font-bold">Events</h1>
+          <IconTriangleInvertedFilled size={12} />
         </div>
-    );
+        <div className="flex gap-2 items-center">
+          <Button variant="light">POPULAR</Button>
+          <ActionIcon
+            variant="white"
+            size="lg"
+            color="dark"
+            aria-label="add feeds"
+          >
+            <IconFilter />
+          </ActionIcon>
+        </div>
+      </div>
+      <div>
+        {events.map((event) => {
+          return <Event key={event.id} event={event} />;
+        })}
+      </div>
+    </div>
+  );
 }
