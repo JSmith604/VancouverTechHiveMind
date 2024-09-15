11ce0c1d8a8c9c09240615f01da0d47f4362368f
tarurata <saycheese5963@gmail.com>
2024-09-14 19:34:06 -0700

    Remove unneccessary line

--- src/Events/Events.jsx
+++ src/Events/Events.jsx
@@ -16,7 +16,7 @@ const events = [
 
 export function Events() {
     return (
-        <div className="w-[400px] p-8 border-r border-gray-200">
+        <div className="w-[400px] p-8">
             <div className="flex justify-between mb-4">
                 <div className="flex gap-2 items-center">
                     <h1 className="text-2xl font-bold">Jobs</h1>
