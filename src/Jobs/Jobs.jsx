29f753ddc5eee880891b3f740f006eafdb7fffda
Iman Mahmoudinasab <imanmahmoudinasab@gmail.com>
2024-09-14 19:14:52 -0700

    add job details cards

--- src/Jobs/Jobs.jsx
+++ src/Jobs/Jobs.jsx
@@ -4,10 +4,11 @@ import { Job } from "./Job";
 import jobs from "../data/jobs.json";
 import { IconTriangleInvertedFilled } from "@tabler/icons-react";
 import { IconFilter } from "@tabler/icons-react";
+import { IconShare } from "@tabler/icons-react";
 
 export function Jobs() {
   return (
-    <div className="w-[400px] p-8 border-r border-gray-200">
+    <div className="w-[550px] p-8 border-r border-gray-200">
       <div className="flex justify-between mb-4">
         <div className="flex gap-2 items-center">
           <h1 className="text-2xl font-bold">Jobs</h1>
