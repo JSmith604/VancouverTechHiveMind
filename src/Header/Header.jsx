ef354a07e9c23fee0a1d46bbf0b2338e92b45465
tarurata <saycheese5963@gmail.com>
2024-09-14 17:45:31 -0700

    Remove Container

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -6,28 +6,26 @@ import classes from "./Header.module.css";
 
 export function Header() {
   return (
-    <header className="p-4 border-b border-gray-200">
-      <Container size="xl" className={classes.inner}>
-        <div className="flex items-center gap-1">
-          <div>
-            <img src={logo} className="h-7 w-7" alt="logo" />
-          </div>
-          <h1 class="text-2xl font-bold">Tech Hive</h1>
+    <header className="p-4 border-b border-gray-200 flex justify-between items-center px-8">
+      <div className="flex items-center gap-1">
+        <div>
+          <img src={logo} className="h-7 w-7" alt="logo" />
         </div>
-        <div className="flex gap-2">
-          <div className="text-right">
-            <p className="text-gray-600 font-bold hover:text-blue-500 transition-colors text-sm">Vancouver, Canada</p>
-            <p className="text-gray-600 hover:text-blue-500 transition-colors text-xs underline">Change city</p>
-          </div>
-          <Button variant="default" style={{ width: 'auto', padding: '0.5rem' }}>
-            <IconLayout className="text-gray-600 hover:text-blue-500 transition-colors" />
-          </Button>
-          <Button style={{ backgroundColor: '#FFE57E', color: 'black', fontSize: '11px' }}>LOGIN / REGISTER</Button>
-          <ActionIcon size="lg" variant="default">
-            <IconDotsVertical size={18} />
-          </ActionIcon>
+        <h1 class="text-2xl font-bold">Tech Hive</h1>
+      </div>
+      <div className="flex gap-2">
+        <div className="text-right">
+          <p className="text-gray-600 font-bold hover:text-blue-500 transition-colors text-sm">Vancouver, Canada</p>
+          <p className="text-gray-600 hover:text-blue-500 transition-colors text-xs underline">Change city</p>
         </div>
-      </Container>
+        <Button variant="default" style={{ width: 'auto', padding: '0.5rem' }}>
+          <IconLayout className="text-gray-600 hover:text-blue-500 transition-colors" />
+        </Button>
+        <Button style={{ backgroundColor: '#FFE57E', color: 'black', fontSize: '11px' }}>LOGIN / REGISTER</Button>
+        <ActionIcon size="lg" variant="default">
+          <IconDotsVertical size={18} />
+        </ActionIcon>
+      </div>
     </header>
   );
 }
